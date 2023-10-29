a90cc4aabd21e5a6e35a8e5d8a419311eaa315ee
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-10-29 01:06:34 -0700

    debug: testing

--- src/main.jsx
+++ src/main.jsx
@@ -4,34 +4,34 @@ import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
-// import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/russell_portfolio/",
-//     element: <App />,
-//     children: [
-//       {
-//         path: "/russell_portfolio/",
-//         element: <Home />,
-//       },
-//       {
-//         path: "/russell_portfolio/contact",
-//         element: <Contact />,
-//       },
-//     ],
-//   },
-// ], {basename: "/russell_portfolio"});
+const router = createBrowserRouter([
+  {
+    path: "/russell_portfolio/",
+    element: <App />,
+    children: [
+      {
+        path: "/russell_portfolio/",
+        element: <Home />,
+      },
+      {
+        path: "/russell_portfolio/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
-const routeElements = (
-  <Route path="/">
-    <Route index element={<Home />}/>
-    <Route path="/contact" element = {<Contact />}/>
-  </Route>
-)
+// const routeElements = (
+//   <Route path="/">
+//     <Route index element={<Home />}/>
+//     <Route path="/contact" element = {<Contact />}/>
+//   </Route>
+// )
 
-const router = createBrowserRouter(createRoutesFromElements(routeElements), {basename: "/russell_portfolio"});
+// const router = createBrowserRouter(createRoutesFromElements(routeElements), {basename: "/russell_portfolio"});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
