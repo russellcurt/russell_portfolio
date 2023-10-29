b3455746bb8faf15faffa8460013a6efeb548cd0
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-10-29 00:54:52 -0700

    testing html

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,34 @@ import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/russell_portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/russell_portfolio/",
-        element: <Home />,
-      },
-      {
-        path: "/russell_portfolio/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/russell_portfolio/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/russell_portfolio/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/russell_portfolio/contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ], {basename: "/russell_portfolio"});
+
+const routeElements = (
+  <Route path="/">
+    <Route index element={<Home />}/>
+    <Route path="/contact" element = {<Contact />}/>
+  </Route>
+)
+
+const router = createBrowserRouter(createRoutesFromElements(routeElements), {basename: "/russell_portfolio"});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
