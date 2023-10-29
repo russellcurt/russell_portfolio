dc8d5102f6e1ff59ff84afff36ab4869d9c7e02d
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-10-29 01:45:30 -0700

    test: routing modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/russell_portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/russell_portfolio/",
-        element: <Home />,
-      },
-      {
-        path: "/russell_portfolio/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/russell_portfolio/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/russell_portfolio/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/russell_portfolio/contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
 
-// const routeElements = (
-//   <Route path="/">
-//     <Route index element={<Home />}/>
-//     <Route path="/contact" element = {<Contact />}/>
-//   </Route>
-// )
+const routeElements = (
+  <Route path="/">
+    <Route index element={<Home />}/>
+    <Route path="/contact" element = {<Contact />}/>
+  </Route>
+)
 
-// const router = createBrowserRouter(createRoutesFromElements(routeElements), {basename: "/russell_portfolio"});
+const router = createBrowserRouter(createRoutesFromElements(routeElements));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
